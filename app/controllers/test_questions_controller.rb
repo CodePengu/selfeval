a67b4d3915d3b70d55a3cfe0250f3ec486548890
EC2 Default User <ec2-user@ip-172-31-13-155.ec2.internal>
2018-04-09 22:20:55 +0000

    Temporary changes for summary feature

--- app/controllers/test_questions_controller.rb
+++ app/controllers/test_questions_controller.rb
@@ -1,9 +1,13 @@
 class TestQuestionsController < ApplicationController
 
+@@score =0
+
   def index
     @questions = Question.all
     @correctness = Hash[@questions.map {|question| [question.id, ""]}]
+    @correct_answer = Hash[@questions.map {|question| [question.id, ""]}]
     @answers = Hash[@questions.map {|question| [question.id, "blank"]}]
+    @count=0
     if params[:answers] != nil
       params[:answers].each do |id, answer|
         id = id.to_i
@@ -13,10 +17,24 @@ class TestQuestionsController < ApplicationController
         elsif (answer != "blank")
           @correctness[id] = ("wrong")
         end
+        @correct_answer[id] =("#{question.answer}")
         @answers[id] = ("#{answer}")
+        if (@correctness[id] == ("correct"))
+         @count=@count+1
+        end
       end
+      @@score = @count
     end
     
   end
+
+
+  def summary
+    @questions = Question.all
+    @count1 = @@score
+    @total =Question.count
+  end
+
+
 
 end
