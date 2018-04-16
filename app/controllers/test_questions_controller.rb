ccbd7a3ddeb56c536c29f2aa747270afdf2cbffa
EC2 Default User <ec2-user@ip-172-31-13-155.ec2.internal>
2018-04-16 20:05:00 +0000

    Check1 with score being declared externally

--- app/controllers/test_questions_controller.rb
+++ app/controllers/test_questions_controller.rb
@@ -1,6 +1,6 @@
 class TestQuestionsController < ApplicationController
 
-
+@@score =0
 
   def index
     @@score =0
@@ -14,13 +14,15 @@ class TestQuestionsController < ApplicationController
         question = Question.find_by(:id => id)
         if (question.answer == answer)
           @correctness[id] = ("correct")
+          @count=@count+1
         elsif (answer != "blank")
           @correctness[id] = ("wrong")
+          @count=@count
         end
         @answers[id] = ("#{answer}")
-        if (@correctness[id] == ("correct"))
-         @count=@count+1
-        end
+       # if (@correctness[id] == ("correct"))
+       #  @count=@count+1
+       # end
       end
       @@score = @count
     end
@@ -29,6 +31,7 @@ class TestQuestionsController < ApplicationController
 
 
   def summary
+    @count1=0
     @questions = Question.all
     @count1 = @@score
     @total =Question.count
