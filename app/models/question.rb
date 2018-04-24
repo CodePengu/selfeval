c861fc2789a1fb31321c78a61339eb4deca9caf2
EC2 Default User <ec2-user@ip-172-31-13-155.ec2.internal>
2018-04-24 22:53:03 +0000

    Added topic validation as a rspec functionality for iteration_4

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,8 @@ class Question < ApplicationRecord
   validates_presence_of :option3
   validates_presence_of :option4
   validates_presence_of :answer
+  validates_presence_of :topic
+
   mount_uploader :image, ImageUploader
  
   private
