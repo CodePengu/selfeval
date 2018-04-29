2f4373fe9a2a96d8a62a6da1c5f390b89be51417
Your Name <you@example.com>
2018-04-29 03:33:29 +0000

    aaa

--- app/controllers/test_questions_controller.rb
+++ app/controllers/test_questions_controller.rb
@@ -6,9 +6,19 @@ class TestQuestionsController < ApplicationController
     @correctness = Hash[@questions.map {|question| [question.id, ""]}]
     @answers = Hash[@questions.map {|question| [question.id, "blank"]}]
     @mark = Hash[@questions.map {|question| [question.id, "~review"]}]
+    session[:selected_topics] = Hash[@questions.map {|question| [question.topic, ""]}]
     @review = params[:review]
     @submitted = params[:submitted]
     @count=0
+
+    if params[:selected_topics] != nil
+      params[:selected_topics].each do |topic,selector|
+        topic = topic.to_s
+        if (selector == "selected")
+          session[:selected_topics][topic] = ("selected")
+        end
+      end
+    end
 
     if params[:mark] != nil
       params[:mark].each do |id,marker|
