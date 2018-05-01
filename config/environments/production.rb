099449024f33c7a50c8c0c59d67757e9bcddec8d
Xien Thomas <xien.thomas@tamu.edu>
2018-05-01 17:13:13 +0000

    fixes to password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'self-eval-2121.herokuapp.com'}#Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => 'selfeval-codepengu-606.herokuapp.com'}#Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
