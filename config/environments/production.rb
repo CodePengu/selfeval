18b18e27c8c8e59970cd4565799b22b81aa7de9d
Xien Thomas <xien.thomas@tamu.edu>
2018-05-01 03:42:21 +0000

    final commits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'https://arcane-meadow-29094.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://selfeval-codepengu-606.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
